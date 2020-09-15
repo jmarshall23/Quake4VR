b95dba91f3b8c0e36a35c513ce8dd7bbfa7f34b3
Justin Marshall <justinmarshall20@gmail.com>
2020-09-15 10:24:01 -0700

    Added command line batch file.

---
+++ test_vr.bat
@@ -0,0 +1,1 @@
+quake4vr.exe  +devmap game/hangar1 +set in_mouse -1 +set r_brightness 1 +set com_allowconsole 1 +set r_fullscreen 0 +set fs_game q4vr +set r_mode -1 +set r_customwidth 1920 +set r_customheight 1080
